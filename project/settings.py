3caf1c447ddc8fb7f82543f86766889d534fae64
JeromeFuselier <jerome.fuselier@free.fr>
2019-11-26 22:45:16 +0100

    Modify ALLOWED_HOSTS for docker container access

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "y7mh)k%0gu=c+btqq@ds1mw$ba+pkwmzut_avx-%0*k-g65xd1"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
